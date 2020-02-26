528cc37d4467814eddad96614a811df5f84eb3dc
Nirmal Pathak <nirmal@avdevs.com>
2020-02-26 16:42:35 +0530

    Adding shebang.

--- sample_app.py
+++ sample_app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # sample_app.py
 import os
 import time
--- test_sample_app.py
+++ test_sample_app.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+
 # test_sample_app.py
+#import os
 from sample_app import app
 
 def test_hello():
     response = app.test_client().get('/')
+    #VER = os.environ.get('VERSION')
+    #HOST = os.uname()[1]
 
     assert response.status_code == 200
-    #assert response.data == b'Hello, World!'
     #assert response.data == b'Hello World!'
-    #assert response.data == b'"Hello World! Running on Host: " + HOST + " Version: " + VER + "\n"'
+    #STRING = "Hello World! Running on Host: " + HOST + " Version: " + VER + "\n"
+    #assert response.data.decode() == STRING
