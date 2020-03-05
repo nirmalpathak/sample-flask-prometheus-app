2cbb4b27be026c4e496db539d35629b8473ba84b
Nirmal Pathak <nirmal@avdevs.com>
2020-03-05 16:36:44 +0530

    Test responses with and without VERSION variable set.

--- src/tests/test_sample_app.py
+++ src/tests/test_sample_app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 # test_sample_app.py
-#import os
+import os
 from sample_app import app
 
 def test_hello():
     response = app.test_client().get('/')
-    #VER = os.environ.get('VERSION')
-    #HOST = os.uname()[1]
+    VER = os.environ.get('VERSION')
+    HOST = os.uname()[1]
+
+    if VER is None:
+        STRING = "Hello World! \n"
+    else:
+        STRING = "Hello World! Running on Host: " + HOST + " Version: " + VER + "\n"
+
 
     assert response.status_code == 200
-    #assert response.data == b'Hello World!'
-    #STRING = "Hello World! Running on Host: " + HOST + " Version: " + VER + "\n"
-    #assert response.data.decode() == STRING
+    assert response.data.decode() == STRING
